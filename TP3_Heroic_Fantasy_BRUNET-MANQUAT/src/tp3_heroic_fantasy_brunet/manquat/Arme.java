2a75df6ea1f3b8313ed95b93dc3ce4661e9a9f4f
Rémi Brunet-Manquat <remi.brunet-manquat@epfedu.fr>
2023-10-18 11:17:58 +0200

    TP3 Heroic Fantasy

---
+++ TP3_Heroic_Fantasy_BRUNET-MANQUAT/src/tp3_heroic_fantasy_brunet/manquat/Arme.java
@@ -0,0 +1,33 @@
+/*
+ * Brunet-Manquat Rémi, TDC, TP3, 18/10/2023
+ */
+package tp3_heroic_fantasy_brunet.manquat;
+
+/**
+ *
+ * @author rembr
+ */
+public class Arme {
+    String nom;
+    int nivAttaque;
+        
+    public Arme(String nom, int nivAttaque){
+        this.nom=nom;
+        if (nivAttaque<=100){
+            this.nivAttaque=nivAttaque;
+        }
+        else{
+            this.nivAttaque=0;
+        }
+    }
+
+    public int getNivAttaque() {
+        return nivAttaque;
+    }
+
+    @Override
+    public String toString() {
+        return "Arme{" + "nom=" + nom + ", nivAttaque=" + nivAttaque + '}';
+    }  
+    
+}
---
+++ TP3_Heroic_Fantasy_BRUNET-MANQUAT/src/tp3_heroic_fantasy_brunet/manquat/TP3_Heroic_Fantasy_BRUNETMANQUAT.java
@@ -0,0 +1,19 @@
+/*
+ * Brunet-Manquat Rémi, TDC, TP3, 18/10/2023
+ */
+package tp3_heroic_fantasy_brunet.manquat;
+
+/**
+ *
+ * @author rembr
+ */
+public class TP3_Heroic_Fantasy_BRUNETMANQUAT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
