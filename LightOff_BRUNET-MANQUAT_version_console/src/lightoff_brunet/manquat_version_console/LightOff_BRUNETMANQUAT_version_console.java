a29d59cb392b90d9724561606bdfca42f6cfe2d8
Rémi Brunet-Manquat <remi.brunet-manquat@epfedu.fr>
2023-10-27 20:31:31 +0200

    Mini projet Light Off : le jeu dans sa version initiale est complet et fonctionnel.

--- LightOff_BRUNET-MANQUAT_version_console/src/lightoff_brunet/manquat_version_console/LightOff_BRUNETMANQUAT_version_console.java
+++ LightOff_BRUNET-MANQUAT_version_console/src/lightoff_brunet/manquat_version_console/LightOff_BRUNETMANQUAT_version_console.java
@@ -13,27 +13,9 @@ public class LightOff_BRUNETMANQUAT_version_console {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        GrilleDeJeu GrilleDeCellules=new GrilleDeJeu(5,5);
-        System.out.println(GrilleDeCellules.toString());
-        
-        
-        
-        
-        GrilleDeCellules.activerDiagonaleMontante();
-        GrilleDeCellules.activerDiagonaleDescendante();
-        GrilleDeCellules.activerLigneDeCellules(0);
-        GrilleDeCellules.activerColonneDeCellules(0);
-        System.out.println("\n"+GrilleDeCellules.toString());
-        
-        
-        System.out.println(GrilleDeCellules.cellulesToutesEteintes());
-        
-        GrilleDeCellules.eteindreToutesLesCellules();
-        System.out.println("\n"+GrilleDeCellules.toString());
-        System.out.println(GrilleDeCellules.cellulesToutesEteintes());
-        
-        GrilleDeCellules.melangerMatriceAleatoirement(10);
-        System.out.println("\n"+GrilleDeCellules.toString());
+        Partie Jeu=new Partie();
+        Jeu.initialiserPartie();
+        Jeu.lancerPartie();
     }
     
 }
--- LightOff_BRUNET-MANQUAT_version_console/src/lightoff_brunet/manquat_version_console/Partie.java
+++ LightOff_BRUNET-MANQUAT_version_console/src/lightoff_brunet/manquat_version_console/Partie.java
@@ -3,6 +3,8 @@
  */
 package lightoff_brunet.manquat_version_console;
 
+import java.util.Scanner;
+
 /**
  * Cette classe permet la création d'une partie de jeu LightOff en utilisant une
  * grille de cellules. Elle permet de jouer au jeu.
@@ -26,15 +28,50 @@ public class Partie {
      * grille de jeu est bien mélangée.
      */
     public void initialiserPartie(){
-        while (this.grille.cellulesToutesEteintes()==false){
+        while (this.grille.cellulesToutesEteintes()==true){
             this.grille.melangerMatriceAleatoirement(10);
         }
     }
     
+    /**
+     * Elle permet de jouer au jeu LightOff en affichant l'état initial de la 
+     * grille, puis en entrant dans une boucle de jeu qui continue tant que 
+     * toutes les cellules ne sont pas éteintes.
+     */
     public void lancerPartie(){
+        Scanner sc= new Scanner(System.in);
         System.out.println(this.grille.toString());
         while(this.grille.cellulesToutesEteintes()==false){
-            
+            System.out.println("\nSelectionner l'action que vous voulez faire. (0 => ligne ; 1 => colonne ; 2 => diagonnale descendante ; 3 => diagonnale montante)");
+            int saisie=sc.nextInt(4);
+            // Les erreurs de saisies utilisateur ne sont pas encore traitées, c'est l'une des premières amélirations qui va être mise en place.
+            if (saisie==0){
+                System.out.println("\nSelectionnez le numéro de la ligne à changer :");
+                int num_ligne=sc.nextInt(7);
+                this.grille.activerLigneDeCellules(num_ligne);
+                this.nbCoups+=1;
+                System.out.println("\nVotre nombre de coups joués est de : "+this.nbCoups);
+                
+            }else if (saisie==1){
+                System.out.println("\nSelectionnez le numéro de la colonne à changer :");
+                int num_colonne=sc.nextInt(7);
+                this.grille.activerColonneDeCellules(num_colonne);
+                this.nbCoups+=1;
+                System.out.println("\nVotre nombre de coups joués est de : "+this.nbCoups);
+                
+            }else if (saisie==2){
+                this.grille.activerDiagonaleDescendante();
+                this.nbCoups+=1;
+                System.out.println("\nVotre nombre de coups joués est de : "+this.nbCoups);
+                
+            }else{
+                this.grille.activerDiagonaleMontante();
+                this.nbCoups+=1;
+                System.out.println("\nVotre nombre de coups joués est de : "+this.nbCoups);
+                
+            }
+            System.out.println("\n"+this.grille.toString());
         }
+        System.out.println("Vous avez réussi ! Votre nombre de coups est de : "+this.nbCoups);
     }
 }
