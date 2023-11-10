9a0df8c4f54b1e6af2ba1a411d386e9dc97ab575
Rémi Brunet-Manquat <remi.brunet-manquat@epfedu.fr>
2023-11-10 23:59:24 +0100

    Mini projet Light Off : debut implementation amélioration niv difficulte

--- LightOff_BRUNET-MANQUAT_version_console/src/lightoff_brunet/manquat_version_console/Partie.java
+++ LightOff_BRUNET-MANQUAT_version_console/src/lightoff_brunet/manquat_version_console/Partie.java
@@ -13,13 +13,14 @@ import java.util.Scanner;
 public class Partie {
     GrilleDeJeu grille;
     int nbCoups;
+    Scanner sc= new Scanner(System.in);
+    
 
     /**
      * Ce constructeur permet de créé une nouvelle instance de la grille de 
      * cellules lumineuses et initialise le compteur nbCoups à zéro.
      */
-    public Partie() {
-        this.grille = new GrilleDeJeu(7,7);
+    public Partie() {        
         this.nbCoups = 0;
     }
     
@@ -27,24 +28,32 @@ public class Partie {
      * Cette méthode permet d’initialiser la partie ; Elle s’assure que la 
      * grille de jeu est bien mélangée.
      */
-    public void initialiserPartie(){
+    public void initialiserPartie(int NivDifficulte){
+        if (NivDifficulte==0){
+          this.grille = new GrilleDeJeu(5,5);  
+        }
+        else if (NivDifficulte==1 || NivDifficulte==3 || NivDifficulte==5){
+            this.grille = new GrilleDeJeu(7,7);
+        }else if (NivDifficulte==2 || NivDifficulte==4){
+            this.grille = new GrilleDeJeu(9,9);
+        }
         while (this.grille.cellulesToutesEteintes()==true){
             this.grille.melangerMatriceAleatoirement(10);
         }
     }
     
-    /**
-     * Elle permet de jouer au jeu LightOff en affichant l'état initial de la 
-     * grille, puis en entrant dans une boucle de jeu qui continue tant que 
-     * toutes les cellules ne sont pas éteintes.
-     */
-    public void lancerPartie(){
-        Scanner sc= new Scanner(System.in);
-        System.out.println(this.grille.toString());
+    
+    public int selectionDifficulte(){
+        System.out.println("Selectionner la difficulté. (0 => Facile ; 1 => Moyenne ; 2 => Difficile ; 3 => Challenge ; 4 => Cauchemar ; 5 => Speedrun");
+        int difficulte=sc.nextInt(6);
+        return difficulte;
+    }
+    
+    public void deroulerPartie(){
+        System.out.println("\n"+this.grille.toString());
         while(this.grille.cellulesToutesEteintes()==false){
             System.out.println("\nSelectionner l'action que vous voulez faire. (0 => ligne ; 1 => colonne ; 2 => diagonnale descendante ; 3 => diagonnale montante)");
             int saisie=sc.nextInt(4);
-            // Les erreurs de saisies utilisateur ne sont pas encore traitées, c'est l'une des premières amélirations qui va être mise en place.
             if (saisie==0){
                 System.out.println("\nSelectionnez le numéro de la ligne à changer :");
                 int num_ligne=sc.nextInt(7);
@@ -73,5 +82,16 @@ public class Partie {
             System.out.println("\n"+this.grille.toString());
         }
         System.out.println("Vous avez réussi ! Votre nombre de coups est de : "+this.nbCoups);
+    }    
+    
+    /**
+     * Elle permet de jouer au jeu LightOff en affichant l'état initial de la 
+     * grille, puis en entrant dans une boucle de jeu qui continue tant que 
+     * toutes les cellules ne sont pas éteintes.
+     */
+    public void lancerPartie(){        
+        int ChoixDifficulte=selectionDifficulte();
+        initialiserPartie(ChoixDifficulte);
+        deroulerPartie();        
     }
 }
