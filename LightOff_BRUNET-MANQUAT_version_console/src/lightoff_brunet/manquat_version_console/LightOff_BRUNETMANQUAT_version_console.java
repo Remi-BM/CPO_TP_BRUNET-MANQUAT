b9565322cd661505231989a1222e4f917a5358fe
Rémi Brunet-Manquat <remi.brunet-manquat@epfedu.fr>
2023-11-11 14:21:38 +0100

    Mini projet Light Off : implementation des modes de difficulté

--- LightOff_BRUNET-MANQUAT_version_console/src/lightoff_brunet/manquat_version_console/LightOff_BRUNETMANQUAT_version_console.java
+++ LightOff_BRUNET-MANQUAT_version_console/src/lightoff_brunet/manquat_version_console/LightOff_BRUNETMANQUAT_version_console.java
@@ -13,16 +13,8 @@ public class LightOff_BRUNETMANQUAT_version_console {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        long debut = System.currentTimeMillis();
-
         Partie Jeu=new Partie();
-        Jeu.lancerPartie();
-
-        long fin = System.currentTimeMillis();
-        long tempsEcoule = fin - debut;
-        
-        System.out.println("Temps écoulé : " + tempsEcoule + " millisecondes");
-        
+        Jeu.lancerPartie();       
     }
     
 }
--- LightOff_BRUNET-MANQUAT_version_console/src/lightoff_brunet/manquat_version_console/Partie.java
+++ LightOff_BRUNET-MANQUAT_version_console/src/lightoff_brunet/manquat_version_console/Partie.java
@@ -13,6 +13,8 @@ import java.util.Scanner;
 public class Partie {
     GrilleDeJeu grille;
     int nbCoups;
+    int nbCoupsMax;
+    int taille;
     Scanner sc= new Scanner(System.in);
     
 
@@ -30,12 +32,29 @@ public class Partie {
      */
     public void initialiserPartie(int NivDifficulte){
         if (NivDifficulte==0){
-          this.grille = new GrilleDeJeu(5,5);  
-        }
-        else if (NivDifficulte==1 || NivDifficulte==3 || NivDifficulte==5){
+          this.grille = new GrilleDeJeu(5,5);
+          this.taille=5;
+          this.nbCoupsMax=-1;
+        }else if (NivDifficulte==1){
             this.grille = new GrilleDeJeu(7,7);
-        }else if (NivDifficulte==2 || NivDifficulte==4){
+            this.taille=7;
+          this.nbCoupsMax=-1;
+        }else if (NivDifficulte==2){
             this.grille = new GrilleDeJeu(9,9);
+            this.taille=9;
+            this.nbCoupsMax=-1;
+        }else if (NivDifficulte==3){
+            this.grille = new GrilleDeJeu(7,7);
+            this.taille=7;
+            this.nbCoupsMax=15;
+        }else if (NivDifficulte==4){
+            this.grille = new GrilleDeJeu(9,9);
+            this.taille=9;
+            this.nbCoupsMax=10;
+        }else{
+            this.grille = new GrilleDeJeu(7,7);
+            this.taille=7;
+            this.nbCoupsMax=-2;
         }
         while (this.grille.cellulesToutesEteintes()==true){
             this.grille.melangerMatriceAleatoirement(10);
@@ -51,37 +70,86 @@ public class Partie {
     
     public void deroulerPartie(){
         System.out.println("\n"+this.grille.toString());
-        while(this.grille.cellulesToutesEteintes()==false){
+        int coupRest=this.nbCoupsMax;
+        boolean modeSpeedrun;
+        long debut=System.currentTimeMillis(); // on l'initialise pour pas avoir d'erreur de syntaxe
+        if (this.nbCoupsMax==-2){
+            modeSpeedrun=true;
+            debut = System.currentTimeMillis();
+        }
+        else{
+            modeSpeedrun=false;
+        }
+        
+        while(this.grille.cellulesToutesEteintes()==false && this.nbCoupsMax!=0){
             System.out.println("\nSelectionner l'action que vous voulez faire. (0 => ligne ; 1 => colonne ; 2 => diagonnale descendante ; 3 => diagonnale montante)");
             int saisie=sc.nextInt(4);
             if (saisie==0){
                 System.out.println("\nSelectionnez le numéro de la ligne à changer :");
-                int num_ligne=sc.nextInt(7);
+                int num_ligne=sc.nextInt(this.taille);
                 this.grille.activerLigneDeCellules(num_ligne);
                 this.nbCoups+=1;
+                this.nbCoupsMax-=1;
+                if (this.nbCoupsMax>0){
+                    coupRest-=1;
+                    System.out.println("\nIl vous reste : "+coupRest+" à jouer !");
+                }
                 System.out.println("\nVotre nombre de coups joués est de : "+this.nbCoups);
                 
             }else if (saisie==1){
                 System.out.println("\nSelectionnez le numéro de la colonne à changer :");
-                int num_colonne=sc.nextInt(7);
+                int num_colonne=sc.nextInt(this.taille);
                 this.grille.activerColonneDeCellules(num_colonne);
                 this.nbCoups+=1;
+                this.nbCoupsMax-=1;
+                if (this.nbCoupsMax>0){
+                    coupRest-=1;
+                    System.out.println("\nIl vous reste : "+coupRest+" à jouer !");
+                }
                 System.out.println("\nVotre nombre de coups joués est de : "+this.nbCoups);
                 
             }else if (saisie==2){
                 this.grille.activerDiagonaleDescendante();
                 this.nbCoups+=1;
+                this.nbCoupsMax-=1;
+                if (this.nbCoupsMax>0){
+                    coupRest-=1;
+                    System.out.println("\nIl vous reste : "+coupRest+" à jouer !");
+                }
                 System.out.println("\nVotre nombre de coups joués est de : "+this.nbCoups);
                 
             }else{
                 this.grille.activerDiagonaleMontante();
                 this.nbCoups+=1;
+                this.nbCoupsMax-=1;
+                if (this.nbCoupsMax>0){
+                    coupRest-=1;
+                    System.out.println("\nIl vous reste : "+coupRest+" à jouer !");
+                }
                 System.out.println("\nVotre nombre de coups joués est de : "+this.nbCoups);
                 
             }
             System.out.println("\n"+this.grille.toString());
         }
-        System.out.println("Vous avez réussi ! Votre nombre de coups est de : "+this.nbCoups);
+        
+        if (modeSpeedrun==true){
+            long fin = System.currentTimeMillis();
+            long tempsEcoule = fin - debut;
+            long minutes = tempsEcoule / (60 * 1000);
+            long secondes = (tempsEcoule % (60 * 1000)) / 1000;                    
+            long millisecondes = tempsEcoule % 1000;
+            String tempsFormate = String.format("%02dmin %02ds %03dms", minutes, secondes, millisecondes);        
+            System.out.println("\nVous avez réussi ! Votre nombre de coups est de : "+this.nbCoups);
+            System.out.println("\nVotre temps est de : "+tempsFormate);
+        }
+        else{
+            if (this.grille.cellulesToutesEteintes()==true){
+               System.out.println("Vous avez réussi ! Votre nombre de coups est de : "+this.nbCoups); 
+            }
+            else{
+               System.out.println("Vous avez perdu !"); 
+            }
+        }        
     }    
     
     /**
